714780c2f64f4298e808b24ab484757a5ed8e9cb
Powei Lin <poweilin1994@gmail.com>
2023-08-12 15:48:41 -0400

    update pyproject

--- src/lsq_solver/auto_diff.py
+++ src/lsq_solver/auto_diff.py
@@ -8,19 +8,19 @@ from num_dual import jacobian
 ROUNDING_ERROR = 1.48e-8
 
 
-def make_jac(name: str, jac_shape, func: callable):
+def make_jac(name: str, jac_shape, func: callable) -> callable:
     if name == "2-point":
         return partial(diff_2point, jac_shape, func)    # partial other parameters since we need a single-input function
     elif name == "3-point":
         return partial(diff_3point, jac_shape, func)
     elif name == "dual":
-        return partial(diff_auto, func)
+        return partial(diff_auto_multiple_variable, func)
     else:
         msg = "Unsupported jacobian function."
         raise ValueError(msg)
 
 
-def diff_2point(jac_shape, func: callable, *variables):
+def diff_2point(jac_shape, func: callable, *variables) -> np.ndarray:
     """
     2-point numeric finite difference. f'(x) = (f(x+h) - f(x)) / h
     Args:
@@ -40,7 +40,7 @@ def diff_2point(jac_shape, func: callable, *variables):
     return jac
 
 
-def diff_3point(jac_shape, func: callable, *variables):
+def diff_3point(jac_shape, func: callable, *variables) -> np.ndarray:
     """
     3-point numeric finite difference. f'(x) = (f(x+h) - f(x-h)) / 2h
     :param jac_shape: tuple
@@ -72,3 +72,25 @@ def diff_auto(func: Callable[[np.ndarray], np.ndarray], variable: np.ndarray):
     auto diff, only one vector input acceptable
     """
     return np.array(jacobian(func, variable)[1], dtype=np.float64)
+
+
+class Bind(partial):
+    """
+    An improved version of partial which accepts Ellipsis (...) as a placeholder
+    """
+
+    def __call__(self, *args, **keywords):
+        keywords = {**self.keywords, **keywords}
+        iargs = iter(args)
+        args = (next(iargs) if arg is ... else arg for arg in self.args)
+        return self.func(*args, *iargs, **keywords)
+
+
+def diff_auto_multiple_variable(func: callable, *variables) -> np.ndarray:
+    jacobian_matrices = []
+    for i, v in enumerate(variables):
+        vs = (vv if j != i else ... for (j, vv) in enumerate(variables))
+        ft = Bind(func, *vs)
+        j = jacobian(ft, v)
+        jacobian_matrices.append(np.array(j[1], dtype=np.float64))
+    return np.hstack(jacobian_matrices)
